09961dcd699bbb9aa92988ca38158fdba7e80140
h44256 <h44256@gmail.com>
2025-04-22 11:28:08 +0800

    完成表單加入資料到TODOS

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,7 +1,19 @@
-function CreateForm() {
+import { useState } from "react";
+
+function CreateForm({addTodo}) {
+
+  const[content, setContent] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addTodo(content);
+    setContent('');
+  }
+
   return (
-    <form className="create-form">
-      <input type="text" placeholder="輸入代辦事項"></input>
+    <form className="create-form" onSubmit={handleSubmit}>
+      <input type="text" placeholder="輸入代辦事項"
+      value={content}
+      onChange={(e)=>{setContent(e.target.value)}}/>
       <button type="submit">加入</button>
     </form>
   );
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,10 +1,24 @@
 import CreateForm from "./CreateForm";
+import Todo from "./Todo";
+import {useState} from "react";
 
 function TodoWrapper() {
+
+  const [todos, setTodos] = useState([
+    {content:'打掃廁所', id: Math.random()},
+    {content:'寫作業', id: Math.random()}
+  ]);
+  const addTodo = (content) => {
+    setTodos([...todos, {content: content, id: Math.random()}]);
+  }
+
   return (
     <div className="wrapper">
       <h1>代辦事項</h1>
-      <CreateForm />
+      <CreateForm addTodo={addTodo}/>
+      {todos.map((todo)=>{
+        return <Todo todo={todo} key={todo.id}/>
+      })}
     </div>
   );
 }

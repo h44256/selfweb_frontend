380f39f09fb7b39f70942a56c8f8cb48bff1da63
h44256 <h44256@gmail.com>
2025-04-23 15:28:09 +0800

    完成整個TodoList的功能

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,39 @@
-function Todo({todo}){
-    return <div className="todo">
-        <p>{todo.content}</p>
-    </div>
+import { MdDelete, MdEdit } from "react-icons/md";
+import "./Todo.css";
+import EditForm from "./EditForm";
+
+function Todo({ todo, deleteTodo, toggleCompleted, toggleIsEditing, editTodo }) {
+
+  return (
+    // <div className="todo completed">
+    todo.isEditing ? (
+      <EditForm todo={todo} editTodo={editTodo}/>
+    ) : (
+      <div className={`todo ${todo.isCompleted ? "completed" : ""}`}>
+        <p
+          onClick={() => {
+            toggleCompleted(todo.id);
+          }}
+        >
+          {todo.content}
+        </p>
+        <div>
+          <MdEdit
+            onClick={() => {
+              toggleIsEditing(todo.id);
+            }}
+            style={{ cursor: "pointer" }}
+          />
+          <MdDelete
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+            className="delete-icon"
+          />
+        </div>
+      </div>
+    )
+  );
 }
 
-export default Todo
+export default Todo;

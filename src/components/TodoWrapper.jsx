98d5f3402f0984ca46b1d2a50b676a900fc81418
h44256 <h44256@gmail.com>
2025-05-03 18:58:59 +0800

    增加localStorage的功能, 利用useEffect去完成這件事

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,16 +1,58 @@
 import CreateForm from "./CreateForm";
 import Todo from "./Todo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const getInitialTodos = () => {
+  const storedTodos = localStorage.getItem("todos");
+  return storedTodos
+    ? JSON.parse(storedTodos)
+    : [
+        {
+          content: "打掃廁所",
+          id: Math.random(),
+          isCompleted: false,
+          isEditing: false,
+        },
+        {
+          content: "寫作業",
+          id: Math.random(),
+          isCompleted: false,
+          isEditing: false,
+        },
+      ];
+};
 
 function TodoWrapper() {
-  const [todos, setTodos] = useState([
-    { content: "打掃廁所", id: Math.random(), 
-      isCompleted: false, isEditing: false },
-    { content: "寫作業", id: Math.random(), 
-      isCompleted: false, isEditing: false },
-  ]);
+  const [todos, setTodos] = useState(getInitialTodos);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  // const [todos, setTodos] = useState([
+  //   {
+  //     content: "打掃廁所",
+  //     id: Math.random(),
+  //     isCompleted: false,
+  //     isEditing: false,
+  //   },
+  //   {
+  //     content: "寫作業",
+  //     id: Math.random(),
+  //     isCompleted: false,
+  //     isEditing: false,
+  //   },
+  // ]);
   const addTodo = (content) => {
-    setTodos([...todos, { content: content, id: Math.random(), isCompleted: false, isEditing:false }]);
+    setTodos([
+      ...todos,
+      {
+        content: content,
+        id: Math.random(),
+        isCompleted: false,
+        isEditing: false,
+      },
+    ]);
   };
   const deleteTodo = (id) => {
     setTodos(
@@ -19,36 +61,47 @@ function TodoWrapper() {
       })
     );
   };
-  const toggleCompleted = (id)=>{
-    setTodos(todos.map((todo)=>{
-      return todo.id === id 
-      ? {...todo, isCompleted: !todo.isCompleted}
-      : todo
-    }))
-  }
-  const toggleIsEditing = (id) =>{
-    setTodos(todos.map((todo)=>{
-      return todo.id === id 
-      ? {...todo, isEditing: !todo.isEditing}
-      : todo
-    }))
-  }
-  const editTodo = (id, newContent) =>{
-    setTodos(todos.map((todo)=>{
-      return todo.id === id 
-      ? {...todo, content: newContent, isEditing: false}
-      : todo
-    }))
-  }
+  const toggleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo;
+      })
+    );
+  };
+  const toggleIsEditing = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo;
+      })
+    );
+  };
+  const editTodo = (id, newContent) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, content: newContent, isEditing: false }
+          : todo;
+      })
+    );
+  };
 
   return (
     <div className="wrapper">
       <h1>代辦事項</h1>
       <CreateForm addTodo={addTodo} />
       {todos.map((todo) => {
-        return <Todo toggleCompleted = {toggleCompleted}
-        toggleIsEditing = {toggleIsEditing} editTodo={editTodo}
-        todo={todo} key={todo.id} deleteTodo={deleteTodo} />;
+        return (
+          <Todo
+            toggleCompleted={toggleCompleted}
+            toggleIsEditing={toggleIsEditing}
+            editTodo={editTodo}
+            todo={todo}
+            key={todo.id}
+            deleteTodo={deleteTodo}
+          />
+        );
       })}
     </div>
   );
